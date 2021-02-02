2bebc2b7d313fe50c2e74c7e78b54ba49e6c8444
IvanZhuro <ivan.zhuro.by@gmail.com>
2021-02-02 10:00:47 +0300

    init: start task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (interA) {
+    if (interA < 0) {
+        interA = interA * -1;        
+    }
+   let joinA = interA.toString().split('').reverse().join('');
+    return Number(joinA);
 }
